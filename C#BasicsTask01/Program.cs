3be1118b0e2df6b91c88efb4a683641a3cf92d8c
mohamedahmeddevx-web <mohamedahmed.devx@gmail.com>
2026-09-03 12:28:43 +0300

    Add project files.

---
+++ C#BasicsTask01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_BasicsTask01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
